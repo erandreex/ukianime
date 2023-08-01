b563101877a8862ef64b0ed569d9901a39ab7e2e
erandreex <ernesto.solis1@utp.ac.pa>
2023-08-01 11:26:10 -0500

    Cambiar nuevo urlServer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    urlServer: 'https://ukianime.herokuapp.com/api/v1',
+    urlServer: 'https://ukianime-backend-kia5-dev.fl0.io/api/v1',
     urlApi: 'https://kitsu.io/api/edge',
 };
